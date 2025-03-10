b9272e0888bed24732fe4f30fe338943875dc7e0
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-10 14:39:23 +0100

    feat(scorecard): Añadir la definicón de color

---
+++ src/main/java/edu/asestatuas/ScoreCard.java
@@ -0,0 +1,15 @@
+package edu.asestatuas;
+
+public class ScoreCard {
+
+    private final String color;
+
+    public ScoreCard(String color) {
+        this.color = color;
+    }
+
+    String getColor() {
+        return this.color;
+    }
+
+}
