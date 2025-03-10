623298f26a61d3f95062713cd2192a22125ab477
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-10 15:01:13 +0100

    feat(scorecard): Añadir la defincion de los boxeadores

--- src/main/java/edu/asestatuas/ScoreCard.java
+++ src/main/java/edu/asestatuas/ScoreCard.java
@@ -3,6 +3,8 @@ package edu.asestatuas;
 public class ScoreCard {
 
     private final String color;
+    private String blueCorner = "";
+    private String redCorner = "";
 
     public ScoreCard(String color) {
         this.color = color;
@@ -10,6 +12,14 @@ public class ScoreCard {
 
     String getColor() {
         return this.color;
+    }
+
+    void setBCorner(String boxerName) {
+        this.blueCorner = boxerName;
+    }
+
+    void setRCorner(String boxerName) {
+        this.redCorner = redCorner;
     }
 
 }
