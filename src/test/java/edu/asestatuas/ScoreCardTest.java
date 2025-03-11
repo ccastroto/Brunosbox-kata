778dda341f2d473b322345c98412886cd76018d3
ccastroto <carloscastroaldrover@gmail.com>
2025-03-11 17:46:09 +0100

    test(scorecard): Añadir casos test de las esquinas de los boxeadores

--- src/test/java/edu/asestatuas/ScoreCardTest.java
+++ src/test/java/edu/asestatuas/ScoreCardTest.java
@@ -9,10 +9,23 @@ public class ScoreCardTest {
 
     private ScoreCard scoreCard;
 
+
     @Test
     public void testColor() {
         scoreCard = new ScoreCard("white");
         assertEquals(scoreCard.getColor(), "white");
+    }
+    @Test
+    public void testSetBCorner() {
+        scoreCard = new ScoreCard("blue");
+        scoreCard.setBCorner("Rocky Balboa");
+        assertEquals("Rocky Balboa", "Rocky Balboa");
+    }
+    @Test
+    public void testSetRCorner() {
+        scoreCard = new ScoreCard("red");
+        scoreCard.setBCorner("Apollo Creed");
+        assertEquals("Apollo Creed", "Apollo Creed");
     }
 
 }
