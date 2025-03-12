1e5f85106f1a5fc8c52d2620045be83678fdfde3
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-12 11:20:54 +0100

    feat(regularround): Añadir las definiciones y definir el RegularRound

---
+++ src/main/java/edu/asestatuas/RegularRound.java
@@ -0,0 +1,15 @@
+package edu.asestatuas;
+
+import edu.asestatuas.Round;
+
+public class RegularRound {
+
+    String roundScore;
+    byte redBoxerScore;
+    byte blueBoxerScore;
+
+    public RegularRound RegularRound(String roundScore) {
+        this.roundScore = roundScore;
+        return this;
+    }
+}
