fe4032f5c5f1b887a1425bab808d0f63c7192d9b
ccastroto <carloscastroaldrover@gmail.com>
2025-03-16 18:56:49 +0100

    test(roundfactory): Añadir los casos test del knockdownRound y PointsDeducted

--- src/test/java/edu/asestatuas/RoundFactoryTest.java
+++ src/test/java/edu/asestatuas/RoundFactoryTest.java
@@ -12,12 +12,27 @@ public class RoundFactoryTest {
     public void setUp() {
         roundFactory = new RoundFactory();
         RoundFactory.getRound("10 - 9");
+        RoundFactory.getRound("9 -10");
+        RoundFactory.getRound("10 - 8");
+        RoundFactory.getRound("8 -10");
+        RoundFactory.getRound("10 - 8 ,1");
+        RoundFactory.getRound("1, 8 - 10");
     }
 
     @Test
-    public void testgetRound() {
+    public void testgetRegularRound() {
         assertEquals("10 - 9", "10 - 9");
         assertEquals("9 - 10", "9 - 10");
+    }
+    @Test
+    public void testgetKnockdownRound() {
+        assertEquals("10 - 8", "10 - 8");
+        assertEquals("8 - 10", "8 - 10");
+    }
+    @Test
+    public void testgetPointsDeductedRound() {
+        assertEquals("10 - 8 ,1", "10 - 8 ,1");
+        assertEquals("1, 8 - 10", "1, 8 - 10");
     }
 
 }
