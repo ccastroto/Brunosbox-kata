c90bdc8a924e8a8ab3524950ff2850e6dedc3860
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-17 14:40:56 +0100

    test(pointsdeducted): Implementar los casos test de "dfleta" para que pase mejor los casos test

--- src/test/java/edu/asestatuas/PointsDeductedTest.java
+++ src/test/java/edu/asestatuas/PointsDeductedTest.java
@@ -12,4 +12,29 @@ public class PointsDeductedTest {
         this.pointsDeducted = new PointsDeducted("10-8,1");
         assertEquals(this.pointsDeducted.getRoundScore(), "10-8,1");
     }
+
+
+    @Test
+    public void replaceTest() {
+        PointsDeducted round = new PointsDeducted("10 - 8 ,1");
+        assertEquals(round.getRoundScore(), "10-8,1");
+        round = new PointsDeducted("1, 8 - 10");
+        assertEquals(round.getRoundScore(), "1,8-10");
+    }
+
+    @Test
+    public void roundScoreToIntBlueTest() {
+        PointsDeducted round = new PointsDeducted("10 - 8 ,1");
+        assertEquals(round.getRoundScore(), "10-8,1");
+        assertEquals(10, round.getRedBoxerScore());
+        assertEquals(8, round.getBlueBoxerScore());
+    }
+
+    @Test
+    public void roundScoreToIntRedTest() {
+        PointsDeducted round = new PointsDeducted("1, 8 - 10");
+        assertEquals(round.getRoundScore(), "1,8-10");
+        assertEquals(8, round.getRedBoxerScore());
+        assertEquals(10, round.getBlueBoxerScore());
+    }
 }
