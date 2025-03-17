b623be2400133820ecd17d6f9d9a31770ea5cfbc
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-17 14:38:29 +0100

    test(regularround): Implementar los casos test de "dfleta" para que pase mejor los casos test

--- src/test/java/edu/asestatuas/RegularRoundTest.java
+++ src/test/java/edu/asestatuas/RegularRoundTest.java
@@ -16,16 +16,37 @@ public class RegularRoundTest {
     @Test
     public void testgetRedBoxerScore() {
         this.regularRound = new RegularRound("10-9");
-        assertEquals(this.regularRound.getRedBoxerScore(),0);
+        assertEquals(this.regularRound.getRedBoxerScore(),10);
     }
     @Test
     public void testgetBlueBoxerScore() {
         this.regularRound = new RegularRound("10-9");
-        assertEquals(this.regularRound.getBlueBoxerScore(),0);
+        assertEquals(this.regularRound.getBlueBoxerScore(),9);
     }
     @Test
     public void testtoString(){
         this.regularRound = new RegularRound("10-9");
-        assertEquals(this.regularRound.toString(),"0 - 0");
+        assertEquals(this.regularRound.toString(),"10 - 9");
+    }
+
+    @Test
+    public void replaceTest() {
+        RegularRound round = new RegularRound("10 - 9");
+        assertEquals(round.getRoundScore(), "10-9");
+    }
+
+    @Test
+    public void roundScoreToIntTest() {
+        RegularRound round = new RegularRound("10 - 9");
+        assertEquals(round.getRoundScore(), "10-9");
+        assertEquals(10, round.getRedBoxerScore());
+        assertEquals(9, round.getBlueBoxerScore());
+    }
+
+    @Test // este para el alumnado
+    public void boxerRoundScoreToIntTest() {
+        RegularRound round = new RegularRound("10 - 9");
+        assertEquals(10, round.getRedBoxerScore());
+        assertEquals(9, round.getBlueBoxerScore());
     }
 }
