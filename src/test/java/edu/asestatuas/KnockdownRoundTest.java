77d0a31e98f0384e47a4eab6d63e6d51b7f22bb4
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-17 14:39:37 +0100

    test(knockdownround): Implementar los casos test de "dfleta" para que pase mejor los casos test

--- src/test/java/edu/asestatuas/KnockdownRoundTest.java
+++ src/test/java/edu/asestatuas/KnockdownRoundTest.java
@@ -12,4 +12,18 @@ public class KnockdownRoundTest {
         this.knockdownRound = new KnockdownRound("10-8");
         assertEquals(this.knockdownRound.getRoundScore(),"10-8");
     }
+
+    @Test
+    public void replaceTest() {
+        KnockdownRound round = new KnockdownRound("10 - 8");
+        assertEquals(round.getRoundScore(), "10-8");
+    }
+
+    @Test
+    public void roundScoreToIntTest() {
+        KnockdownRound round = new KnockdownRound("10 - 8");
+        assertEquals(round.getRoundScore(), "10-8");
+        assertEquals(10, round.getRedBoxerScore());
+        assertEquals(8, round.getBlueBoxerScore());
+    }
 }
