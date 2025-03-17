613f60b45e13866faa307ee725d083c1f4434acc
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-17 14:35:56 +0100

    test(scorecard): Implementar los casos test de "dfleta" para que paser mejor los casos test

--- src/test/java/edu/asestatuas/ScoreCardTest.java
+++ src/test/java/edu/asestatuas/ScoreCardTest.java
@@ -1,57 +1,122 @@
 package edu.asestatuas;
 
-import edu.asestatuas.ScoreCard;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ScoreCardTest {
 
-    private ScoreCard scoreCard;
+    private ScoreCard card;
 
+    private String[] whiteScoreCard =  {"9 - 10",
+            "9 - 10",
+            "9 - 10",
+            "9 - 10",
+            "10 - 9",
+            "10 - 9",
+            "10 - 9",
+            "9 - 10",
+            "10 - 9",
+            "9 - 10"};
+
+    private String[] pinkScoreCard = {"9 - 10",
+            "9 - 10",
+            "9 - 10",
+            "1, 8 - 10", // referee point deduction
+            "10 - 8",    // knockdown
+            "10 - 8 ,1", // referee point deduction
+            "10 - 9",
+            "9 - 10",
+            "10 - 9",
+            "10 - 8"};
+
+    @Before
+    public void setup() {
+        card = new ScoreCard("white");
+    }
 
     @Test
     public void testColor() {
-        scoreCard = new ScoreCard("white");
-        assertEquals(scoreCard.getColor(), "white");
+        assertEquals(card.getColor(), "white");
     }
+
     @Test
     public void testSetBCorner() {
-        scoreCard = new ScoreCard("blue");
-        scoreCard.setBCorner("Rocky Balboa");
+        card = new ScoreCard("blue");
+        card.setBCorner("Rocky Balboa");
         assertEquals("Rocky Balboa", "Rocky Balboa");
     }
     @Test
     public void testSetRCorner() {
-        scoreCard = new ScoreCard("red");
-        scoreCard.setBCorner("Apollo Creed");
+        card = new ScoreCard("red");
+        card.setBCorner("Apollo Creed");
         assertEquals("Apollo Creed", "Apollo Creed");
     }
     @Test
     public void testGetNumRounds() {
-        scoreCard = new ScoreCard("blue");
-        assertEquals(scoreCard.getNumRounds(), 0);
+        card = new ScoreCard("blue");
+        assertEquals(card.getNumRounds(), 0);
     }
     @Test
     public void testloadJudgeScoreCard() {
-        scoreCard = new ScoreCard("blue");
+        card = new ScoreCard("blue");
         String[] judgeScoreCard = {"10-9", "9-10"};
-        scoreCard.loadJudgeScoreCard(judgeScoreCard);
-        assertEquals(scoreCard.getNumRounds(), 0);
-        assertEquals(0, scoreCard.getNumRounds());
+        card.loadJudgeScoreCard(judgeScoreCard);
+        assertEquals(card.getNumRounds(), 0);
+        assertEquals(0, card.getNumRounds());
     }
     @Test
     public void testgetRedBoxerScoreFinal() {
-        scoreCard = new ScoreCard("red");
+        card = new ScoreCard("red");
         String[] redBoxerScoreFinal = {"10-9", "9-10"};
-        scoreCard.loadJudgeScoreCard(redBoxerScoreFinal);
-        assertEquals(scoreCard.getRedBoxerScoreFinal(), 0);
+        card.loadJudgeScoreCard(redBoxerScoreFinal);
+        assertEquals(card.getRedBoxerScoreFinal(), 0);
     }
     @Test
     public void testgetBlueBoxerScoreFinal() {
-        scoreCard = new ScoreCard("blue");
+        card = new ScoreCard("blue");
         String[] blueBoxerScoreFinal = {"10-9", "9-10"};
-        scoreCard.loadJudgeScoreCard(blueBoxerScoreFinal);
-        assertEquals(scoreCard.getBlueBoxerScoreFinal(), 0);
+        card.loadJudgeScoreCard(blueBoxerScoreFinal);
+        assertEquals(card.getBlueBoxerScoreFinal(), 0);
+    }
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryRegularTest() {
+
+        card.loadJudgeScoreCard(whiteScoreCard);
+        assertEquals(10, card.getNumRounds());
+        assertTrue(card.getRounds().stream().allMatch(a -> a instanceof RegularRound));
+    }
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryNullTest() {
+        card.loadJudgeScoreCard(new String[]{null, null});
+        assertEquals(0, card.getNumRounds());
+    }
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryPointsDeductedTest() {
+
+        card.loadJudgeScoreCard(pinkScoreCard);
+        assertEquals(10, card.getNumRounds());
+        assertEquals(2, card.getRounds().stream().filter(a -> a instanceof PointsDeducted).count());
+    }
+
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryKnockDownTest() {
+
+        card.loadJudgeScoreCard(pinkScoreCard);
+        assertEquals(10, card.getNumRounds());
+        assertEquals(2, card.getRounds().stream().filter(a -> a instanceof KnockdownRound).count());
+    }
+
+    @Test
+    public void getBoxerFinalScoreTest() {
+        assertEquals(0, card.getRedBoxerScoreFinal());
+        assertEquals(0, card.getBlueBoxerScoreFinal());
+        card.loadJudgeScoreCard(whiteScoreCard);
+        assertEquals(94, card.getRedBoxerScoreFinal());
+        assertEquals(96, card.getBlueBoxerScoreFinal());
     }
 }
